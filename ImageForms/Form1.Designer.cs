981bc028477cf8ba5924c66184f2b2564c1818d8
Jesner <105526532+codejesner@users.noreply.github.com>
2022-06-30 19:04:35 -0300

    Initial commit

---
+++ ImageForms/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace ImageForms
+{
+    partial class App
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblSobrenome = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.txtSobrenome = new System.Windows.Forms.TextBox();
+            this.txtAnoNascimento = new System.Windows.Forms.TextBox();
+            this.lblAnoNascimento = new System.Windows.Forms.Label();
+            this.lblMensagem = new System.Windows.Forms.Label();
+            this.pibImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pibImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(36, 32);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(51, 20);
+            this.lblNome.TabIndex = 2;
+            this.lblNome.Text = "Nome";
+            // 
+            // lblSobrenome
+            // 
+            this.lblSobrenome.AutoSize = true;
+            this.lblSobrenome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSobrenome.Location = new System.Drawing.Point(36, 120);
+            this.lblSobrenome.Name = "lblSobrenome";
+            this.lblSobrenome.Size = new System.Drawing.Size(92, 20);
+            this.lblSobrenome.TabIndex = 3;
+            this.lblSobrenome.Text = "Sobrenome";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(40, 56);
+            this.txtNome.Multiline = true;
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(363, 46);
+            this.txtNome.TabIndex = 4;
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(40, 295);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(117, 37);
+            this.btnVerificar.TabIndex = 6;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(163, 295);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(117, 37);
+            this.btnLimpar.TabIndex = 7;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(286, 295);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(117, 37);
+            this.btnSair.TabIndex = 8;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // txtSobrenome
+            // 
+            this.txtSobrenome.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSobrenome.Location = new System.Drawing.Point(40, 143);
+            this.txtSobrenome.Multiline = true;
+            this.txtSobrenome.Name = "txtSobrenome";
+            this.txtSobrenome.Size = new System.Drawing.Size(363, 46);
+            this.txtSobrenome.TabIndex = 9;
+            // 
+            // txtAnoNascimento
+            // 
+            this.txtAnoNascimento.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAnoNascimento.Location = new System.Drawing.Point(40, 228);
+            this.txtAnoNascimento.Multiline = true;
+            this.txtAnoNascimento.Name = "txtAnoNascimento";
+            this.txtAnoNascimento.Size = new System.Drawing.Size(363, 46);
+            this.txtAnoNascimento.TabIndex = 10;
+            // 
+            // lblAnoNascimento
+            // 
+            this.lblAnoNascimento.AutoSize = true;
+            this.lblAnoNascimento.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnoNascimento.Location = new System.Drawing.Point(36, 205);
+            this.lblAnoNascimento.Name = "lblAnoNascimento";
+            this.lblAnoNascimento.Size = new System.Drawing.Size(146, 20);
+            this.lblAnoNascimento.TabIndex = 11;
+            this.lblAnoNascimento.Text = "Ano de nascimento";
+            // 
+            // lblMensagem
+            // 
+            this.lblMensagem.AutoSize = true;
+            this.lblMensagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMensagem.Location = new System.Drawing.Point(40, 379);
+            this.lblMensagem.Name = "lblMensagem";
+            this.lblMensagem.Size = new System.Drawing.Size(0, 24);
+            this.lblMensagem.TabIndex = 12;
+            // 
+            // pibImage
+            // 
+            this.pibImage.Location = new System.Drawing.Point(427, 56);
+            this.pibImage.Name = "pibImage";
+            this.pibImage.Size = new System.Drawing.Size(450, 276);
+            this.pibImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pibImage.TabIndex = 13;
+            this.pibImage.TabStop = false;
+            // 
+            // App
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 449);
+            this.Controls.Add(this.pibImage);
+            this.Controls.Add(this.lblMensagem);
+            this.Controls.Add(this.lblAnoNascimento);
+            this.Controls.Add(this.txtAnoNascimento);
+            this.Controls.Add(this.txtSobrenome);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblSobrenome);
+            this.Controls.Add(this.lblNome);
+            this.Name = "App";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pibImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblSobrenome;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.TextBox txtSobrenome;
+        private System.Windows.Forms.TextBox txtAnoNascimento;
+        private System.Windows.Forms.Label lblAnoNascimento;
+        private System.Windows.Forms.Label lblMensagem;
+        private System.Windows.Forms.PictureBox pibImage;
+    }
+}
+
---
+++ ImageForms/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageForms
+{
+    public partial class App : Form
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+
+            if (txtNome.Text != "" && txtSobrenome.Text != "" && txtAnoNascimento.Text.Length == 4)
+            {
+                string nome = txtNome.Text;
+                string sobrenome = txtSobrenome.Text;
+                int anoNascimento = int.Parse(txtAnoNascimento.Text);
+                int idade = int.Parse(DateTime.Now.ToString("yyyy")) - anoNascimento;
+
+
+                if (idade >= 18)
+                {
+                    lblMensagem.Text ="Bem-vindo " + nome.Replace(nome[0].ToString(), nome[0].ToString().ToUpper()) +
+                        " " +
+                        sobrenome.Replace(sobrenome[0].ToString(), sobrenome[0].ToString().ToUpper()) + " " + idade + " anos";
+                    pibImage.Image = Properties.Resources.aberto;
+                } 
+                else
+                {
+                    lblMensagem.Text = "Acesso Negado!";
+                    pibImage.Image = Properties.Resources.fechado;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Insira as informações corretamente.");
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtNome.Clear();
+            txtSobrenome.Clear();
+            txtAnoNascimento.Clear();
+            lblMensagem.Text = "";
+            pibImage.Image = null;
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
+
+
